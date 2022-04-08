af2d5f5a6b519e33f4cf0b120e0a85ccbf6130d1
Porcescu Dmitri <71259338+Urarak234@users.noreply.github.com>
2022-04-08 22:11:41 +0300

    Create showData.vue

---
+++ showData/showData.vue
@@ -0,0 +1,99 @@
+<template>
+
+    <div id="mainDiv">
+        <div class="list">
+            <ul>
+                <li id="0" @click="showInfo">Person 1</li>
+                <li id="1" @click="showInfo">Person 2</li>
+                <li id="2" @click="showInfo">Person 3</li>
+                <li id="3" @click="showInfo">Person 4</li>
+                <li id="4" @click="showInfo">Porson 5</li>
+            </ul>
+        </div>
+        <div id="info">
+
+        </div>
+        
+    </div>
+    
+</template>
+
+<script>
+export default ({
+    data() {
+      return {       
+        persons : [
+            {
+                name : "Martin",
+                surName : "Garix",
+                age : 22,
+                ocupation : "Beat-maker, Compositor, DJ",
+                email : "dJ_garix@gmail.com"
+            },
+            {
+                name : "Jhony",
+                surName : "Singelton",
+                age : 28,
+                ocupation : "Vocalist, Street Artist",
+                email : "the_three_days_grees_forever@gmail.com"
+            },
+            {
+                name : "Lucy",
+                surName : "Mc-Claren",
+                age : 19,
+                ocupation : "Art-creator, Designer, 2d-Game-maker",
+                email : "littleMagic@gmail.com"
+            },
+            {
+                name : "MJ",
+                surName : "Brown",
+                age : 26,
+                ocupation : "Programmer, Freelancer",
+                email : "structDC@bord.si"
+            },
+            {
+                name : "Mike",
+                surName : "Kovalski",
+                age : 999,
+                ocupation : "Scaring Kids",
+                email : "monster_corporation@gmail.com"
+            }
+        ]
+      }  
+    },
+    methods: {
+        showInfo(event){
+            let leftDiv = document.getElementById("info");
+            leftDiv.innerHTML = "";
+            for (const key in this.persons) {
+                if (key == event.target.id) {
+                    for (const par in this.persons[key]) {
+                        console.log(par," : ",this.persons[key][par]);
+                        let textInfo = document.createElement("p");
+                        textInfo.textContent = `${par} : ${this.persons[key][par]}`;
+                        leftDiv.appendChild(textInfo);
+                    }                  
+                }
+            }
+        }    
+    },
+    computed: {
+    
+    }
+})
+</script>
+
+<style scoped>
+li{
+    list-style: none;
+}
+input{
+    width: 100px;
+}
+#mainDiv{
+    display: flex;
+    width: 100%;
+    justify-content: space-around;
+    align-items: center;
+}
+</style>
